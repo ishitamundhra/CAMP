4a6b63595ec34876117e42ee2dba8bce26790ba3
Ishita Mundhra <ishitamundhra0916@gmail.com>
2021-04-06 00:06:04 +0530

    model basics

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 const express = require ('express');
-const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+const { Console } = require('console');
+const Campground = require('./models/campground');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', 
+   { useNewUrlParser : true,
+    useCreateIndex : true,
+    useUnigfiedTopology : true
+   })
+
+mongoose.connection.on("error",console.error.bind(Console,"Connection Error"));
+  
+mongoose.connection.once("open", () =>
+{
+    console.log("mongoose is connected")
+});
+
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views',path.join(__dirname,'views'));
@@ -9,6 +26,14 @@ app.get('/',(req , res)=>
 {
     res.render('home');
 })
+
+app.get('/main',async (req , res)=>
+{
+    const camp = new Campground({title: 'My Backyard', description:'Cheap campground'});
+    await camp.save();
+    res.send(camp);
+})
+
 
 app.listen(3000,()=>
 {
