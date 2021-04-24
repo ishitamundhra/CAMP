fa378087a38708d1f336117d6824083e258bf87e
Ishita Mundhra <ishitamundhra0916@gmail.com>
2021-04-25 03:03:51 +0530

    Basic Styling

--- seeds/index.js
+++ seeds/index.js
@@ -2,32 +2,35 @@ const mongoose = require('mongoose');
 const { Console } = require('console');
 const Campground = require('../models/campground');
 const cities = require('./cities');
-const {places, descriptors} = require('./seedHelper');
+const { places, descriptors } = require('./seedHelper');
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', 
-   { useNewUrlParser : true,
-    useCreateIndex : true,
-    useUnigfiedTopology : true
-   })
+mongoose.connect('mongodb://localhost:27017/yelp-camp',
+    {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnigfiedTopology: true
+    })
 
-mongoose.connection.on("error",console.error.bind(Console,"Connection Error"));
-  
-mongoose.connection.once("open", () =>
-{
+mongoose.connection.on("error", console.error.bind(Console, "Connection Error"));
+
+mongoose.connection.once("open", () => {
     console.log("mongoose is connected")
 });
 
 const sample = array => array[Math.floor(Math.random() * array.length)];
 
 
-const seedDB = async() => {
+const seedDB = async () => {
     await Campground.deleteMany({});
-    for(let i =0 ;i<50;i++)
-    {
+    for (let i = 0; i < 50; i++) {
+        const price = Math.floor(Math.random() * 20) + 10;
         const random1000 = Math.floor(Math.random() * 1000);
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            price: price
         })
 
         await camp.save();
